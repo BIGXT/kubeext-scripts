fd9caecd43d450a26411f589963158461fbb710c
syswu <root@localhost.localdomain>
2019-03-27 01:55:35 -0400

    init ovs

--- syspods/kube-networks/ovs/build.sh
+++
@@ -1,9 +0,0 @@
-#############################################
-##
-## Copyright (2019, ) Institute of Software
-##     Chinese Academy of Sciences
-##      wuheng@otcaix.iscas.ac.cn
-##
-############################################
-
-docker build . -t iscas-cloudplus.github.io/ovs/node:v2.10.1
