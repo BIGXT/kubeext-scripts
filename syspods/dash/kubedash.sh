12c41dae4b6764f08d87d3397845fffcb6d08cd2
syswu <wuheng@iscas.ac.cn>
2019-03-30 23:19:46 -0400

    fix no roles bug

--- syspods/dash/kubedash.sh
+++ syspods/dash/kubedash.sh
@@ -9,9 +9,11 @@
 function setupDashboard()
 {
   kubectl create -f yamls/kubedash.yaml
+  kubectl create -f yamls/kubedash-admin.yaml
+  kubectl create -f yamls/kubedash-role.yaml
   port=$(kubectl -n kube-system get service kubernetes-dashboard | awk 'NR>1 {print$5}'| awk -F"/" '{print$1}' | awk -F":" '{print$2}')
   echo "URL: https://<masterip>:"$port  
-  echo $(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-token | awk '{print $1}') | grep "token:") 
+  echo $(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') | grep "token:") 
 }
 
 setupDashboard
