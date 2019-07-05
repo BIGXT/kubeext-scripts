e416e099fc73472e2ea8638e675f29e3b13d733f
syswu <wuheng@iscas.ac.cn>
2019-07-05 10:08:06 -0400

    init script

---
+++ solutions/container/dev.sh
@@ -0,0 +1,81 @@
+############################################
+##
+## Copyright (2019, ) Institute of Software
+##      Chinese Academy of Sciences
+##       wuheng@otcaix.iscas.ac.cn
+##
+############################################
+
+version=$(kubeadm version | awk -F"GitVersion:\"" '{print$2}' | awk -F"\"" '{print$1}')
+
+KEY=k8s.gcr.io
+VALUE1=index.alauda.cn/alaudak8s
+VALUE2=gcr.azk8s.cn/google_containers
+
+function download()
+{
+  name=${line//$1/$2}
+  echo docker pull $name
+  docker pull $name
+  docker tag $name $3
+  docker rmi $name
+}
+
+
+function pullImages()
+{
+  while read line
+  do
+    img=$(echo $line | awk -F":" '{print$1}')
+    ver=$(echo $line | awk -F":" '{print$2}')
+    res=$(docker images | grep "$img" | grep "$ver" | grep -v grep)
+    if [[ -z $res ]]
+    then
+      download $KEY $VALUE1 $line
+      download $KEY $VALUE2 $line
+    fi
+  done < images.conf
+}
+
+function setupCluster()
+{
+  swapoff -a
+  
+  res=$(cat /etc/sysctl.conf | grep swappiness)
+  if [[ -z $res ]]
+  then
+    echo "vm.swappiness = 0">> /etc/sysctl.conf 
+  fi
+
+  echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+  rm -rf $HOME/.kube
+  systemctl enable kubelet
+  systemctl start kubelet
+  
+  echo "kubeadm init --kubernetes-version=$version --token-ttl=0"
+  kubeadm init --kubernetes-version=$version --token-ttl=0 
+  
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+  kubectl taint nodes --all node-role.kubernetes.io/master-
+  iptables -P FORWARD ACCEPT
+}
+
+function setupOvs()
+{
+  host=$(hostname)
+  yum install -y openvswitch libibverbs -y
+  systemctl start openvswitch
+  systemctl enable openvswitch
+  kubectl label node $host kube-ovn/role=master
+  #kubectl apply -f yamls/kubeovs.yaml
+  kubectl apply -f https://raw.githubusercontent.com/alauda/kube-ovn/master/yamls/ovn.yaml
+  kubectl apply -f https://raw.githubusercontent.com/alauda/kube-ovn/master/yamls/kube-ovn.yaml
+}
+
+pullImages
+setupCluster
+setupOvs
