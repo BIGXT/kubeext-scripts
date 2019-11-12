ff5b4ab5301d8a3f35e828d553a3fceef294436b
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2019-11-12 16:33:18 +0800

    repo

---
+++ pods/kubernetes/installAll.sh
@@ -0,0 +1,9 @@
+rm -rf /etc/yum.repos.d
+mkdir /etc/yum.repos.d
+mv ./CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
+mv ./kubernetes.repo /etc/yum.repos.d/kubernetes.repo
+yum makecache
+yum -y install kubelet kubeadm kubectl
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+
---
+++ pods/kubernetes/removeAll.sh
@@ -0,0 +1,3 @@
+yum -y remove docker-ce
+rm -rf /var/lib/docker
+yum -y remove kubelet kubeadm kubectl
