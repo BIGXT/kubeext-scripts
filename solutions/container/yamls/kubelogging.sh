3209b8488e646269ade06bc696f070af9cb408c6
syswu <wuheng@iscas.ac.cn>
2019-11-18 09:57:12 +0800

    delete

--- solutions/container/yamls/kubelogging.sh
+++
@@ -1,5 +0,0 @@
-# curl -L https://github.com/kubernetes/kompose/releases/download/v1.18.0/kompose-linux-amd64 -o kompose
-
-wget https://raw.githubusercontent.com/grafana/loki/master/production/docker-compose.yaml
-
-sed -i "s/namespace: monitoring/namespace: kube-system/g" kubemonitor.yaml
--- solutions/container/yamls/kuberegister.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/ContainerSolutions/trow/master/install/trow.yaml
-
-mv trow.yaml kuberegister.yaml
-
-sed -i "s/namespace: kube-public/namespace: kube-system/g" kuberegister.yaml
