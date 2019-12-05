d12cef6b6a47439476cb5b7cb0ef629e9ad25357
syswu <wuheng@iscas.ac.cn>
2019-12-05 16:36:04 +0800

    update

---
+++ wget_arm64.sh
@@ -0,0 +1,3 @@
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubeadm_1.5.6-00_arm64_2929e20e9afe590374ae55cd6dd31fda8ce61f7309e0d4fb171fdadd645cfe1c.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubectl_1.5.6-00_arm64_d61df6cb4de318b91394d8ac3dca12b9ac4e8e9fc2b2fd6183d31ee39572209f.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubelet_1.5.6-01_arm64_76fa0dd62597b79f3dd88c2431d3f1e882d8ca9c7875bcffcc895ad8ac76f9b0.deb
