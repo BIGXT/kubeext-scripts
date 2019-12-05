97b7a5cb6f10e42a4886fc9f67b463b21dea15c5
Heng WU <43158397+syswu@users.noreply.github.com>
2019-12-05 16:59:30 +0800

    Update wget_arm64.sh

--- wget_arm64.sh
+++ wget_arm64.sh
@@ -1,3 +1,5 @@
-wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubeadm_1.5.6-00_arm64_2929e20e9afe590374ae55cd6dd31fda8ce61f7309e0d4fb171fdadd645cfe1c.deb
-wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubectl_1.5.6-00_arm64_d61df6cb4de318b91394d8ac3dca12b9ac4e8e9fc2b2fd6183d31ee39572209f.deb
-wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubelet_1.5.6-01_arm64_76fa0dd62597b79f3dd88c2431d3f1e882d8ca9c7875bcffcc895ad8ac76f9b0.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubeadm_1.14.7-00_arm64_d157ccddc7bf5201b46d115eebc3febc61318342874f933c47c45c9c7e861288.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubectl_1.14.7-00_arm64_47303c24f054a8ed473af8269d9a9bf071bf77c02537937ba63d4eb92e90a052.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubelet_1.14.7-00_arm64_91c37f9dbfd02123527a8e89bd0c06f4ee3eba91586e07452fed756af20b94e7.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/kubernetes-cni_0.7.5-00_arm64_16f686a176ee62fc4f960fd4b272e5e26c73fcced8bd1f8ce9a68a54b2b07e28.deb
+wget https://mirrors.aliyun.com/kubernetes/apt/pool/cri-tools_1.13.0-00_arm64_551fb3bc0ac49efe6a2fd37e0c3c081290c661353055d5c933f41d440ca0c7bd.deb
