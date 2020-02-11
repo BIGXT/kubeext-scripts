f57f16573444f9f9966dea2cda031df935d4064f
wuyuewen <suhupt_f@126.com>
2020-02-11 21:02:21 +0800

    before join

--- scripts/kubernetes/join.sh
+++ scripts/kubernetes/join.sh
@@ -2,7 +2,7 @@
 ##
 ## Copyright (2019, ) Institute of Software
 ##     Chinese Academy of Sciences
-##      wuheng@otcaix.iscas.ac.cn
+##      {wuheng,xuyuanjia2017}@otcaix.iscas.ac.cn
 ##
 ###########################################
 
