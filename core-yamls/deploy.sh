6e49ae8aaa62f08de7c5e9319342faef6bf55030
794774196@qq.com <794774196@qq.com>
2020-04-03 14:41:44 +0800

    support helm 3.1.2

--- core-yamls/deploy.sh
+++ core-yamls/deploy.sh
@@ -34,6 +34,12 @@ function setupCluster()
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
   iptables -P FORWARD ACCEPT
 
+  wget -P /usr/bin http://39.106.124.113/edgecloud/corecloud/helm
+  helm repo update
+  #curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm repo add stable https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+
   kubectl apply -f yamls/
 }
 
